7db009c80c640578c40e126737158c82b4a0dac7
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2026-02-21 01:12:10 +0500

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,17 +4,14 @@ function emit_pusher_notification($channel, $event, $data)
 {
     try {
         $pusher = new \Pusher\Pusher(
-            env('PUSHER_APP_KEY'),
-            env('PUSHER_APP_SECRET'),
-            env('PUSHER_APP_ID'),
-            [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true
-            ]
+            config('broadcasting.connections.pusher.key'),
+            config('broadcasting.connections.pusher.secret'),
+            config('broadcasting.connections.pusher.app_id'),
+            config('broadcasting.connections.pusher.options')
         );
 
         $pusher->trigger($channel, $event, $data);
-        \Log::info('Pusher Works.');
+        // \Log::info('Pusher Works.');
 
         return true;
     } catch (\Exception $e) {
